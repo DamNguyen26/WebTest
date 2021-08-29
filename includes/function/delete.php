bf7f1ee3c057ba3a5284699f8ee9eec8cd5b0c1e
_damnguyen2604_ <dammit2525@yahoo.com.vn>
2021-08-29 12:54:49 +0700

    Update delete.php

--- includes/function/delete.php
+++ includes/function/delete.php
@@ -1,18 +1,18 @@
 <?php
-require_once ("connection.php");
+require_once "connection.php";
 
-if (isset($_REQUEST['delete'])){
+if (isset($_REQUEST["delete"])) {
     $sql = "DELETE FROM MyGuests WHERE id = ?";
     $stmt = $conn->prepare($sql);
     if ($stmt) {
         $stmt->bind_param("i", $id);
-        $id = $_REQUEST['id'];
-    $stmt->execute();
-    echo "Record deleted successfully";
-    }else {
-      echo "Error deleting record: " . $conn->error;
+        $id = $_REQUEST["id"];
+        $stmt->execute();
+        echo "Record deleted successfully";
+    } else {
+        echo "Error deleting record: " . $conn->error;
     }
     $stmt->close();
     $conn->close();
-    }
+}
 ?>
