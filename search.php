191ea26e90c210d6e7633d00ebbab41c7003c16f
_damnguyen2604_ <dammit2525@yahoo.com.vn>
2021-08-29 15:08:45 +0700

    Update search.php

--- search.php
+++ search.php
@@ -1,44 +1,59 @@
-
+</html>
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-<style>
-table, th, td {
-  border: 1px solid black;
-}
-</style>
+    <?php require_once "includes/function/connection.php"; ?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
+
 <body>
+    <?php
+    $sql = "SELECT id, firstname, lastname, email FROM MyGuests WHERE lastname='Doe'";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_result($id, $firstname, $lastname, $email);
+    $stmt->execute();
+    $stmt->store_result();
 
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "myDBTest";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT id, firstname, lastname FROM MyGuests WHERE lastname='Doe'";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  echo "<table><tr><th>ID</th><th>Name</th></tr>";
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "<tr><td>" . $row["id"]. "</td><td>" . $row["firstname"]. " " . $row["lastname"]. "</td></tr>";
-  }
-  echo "</table>";
-} else {
-  echo "0 results";
-}
-
-$conn->close();
-?>
-
+    if ($stmt->num_rows > 0) {
+        echo "<table>";
+        echo "<theader>";
+        echo "<tr>";
+        echo "<th>Id</th>";
+        echo "<th>First name</th>";
+        echo "<th>Last name</th>";
+        echo "<th>Email</th>";
+        echo "<th>Edit</th>";
+        echo "<th>Delete</th>";
+        echo "</tr>";
+        echo "</theader>";
+        echo "<tbody>";
+        // output data of each row
+        while ($stmt->fetch()) {
+            echo "<tr>";
+            echo "<td>" . $id . "</td>";
+            echo "<td>" . $firstname . "</td>";
+            echo "<td>" . $lastname . "</td>";
+            echo "<td>" . $email . "</td>";
+            echo '<td><form action="formUpdate.php" method="post"><input type="hidden" name = "id" value =' .
+                $id .
+                '><input type = "submit" value="Edit" name = "edit"></form></td>';
+            echo '<td><form action="includes/function/delete.php" method="post"><input type="hidden" name = "id" value =' .
+                $id .
+                '><input type = "submit" value="Delete" name = "delete"></form></td>';
+            echo "</tr>";
+        }
+        echo "</tbody>";
+        echo "</table>";
+    } else {
+        echo "0 results";
+    }
+    $stmt->close();
+    $conn->close();
+    ?>
 </body>
+
 </html>
