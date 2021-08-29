0a5db9c17313bb6c4da958edff0afc4fef72ce69
_damnguyen2604_ <dammit2525@yahoo.com.vn>
2021-08-29 12:54:40 +0700

    Update add.php

--- includes/function/add.php
+++ includes/function/add.php
@@ -1,18 +1,19 @@
 <?php
-require_once ("connection.php");
+require_once "connection.php";
 
 // Prepare and bind
-$stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (?, ?, ?)");
+$stmt = $conn->prepare(
+    "INSERT INTO MyGuests (firstname, lastname, email) VALUES (?, ?, ?)"
+);
 $stmt->bind_param("sss", $firstname, $lastname, $email);
 
 // Set parameters and execute
-$firstname = $_POST['name'];
-$lastname = $_POST['comment'];
-$email = $_POST['email'];
+$firstname = $_POST["name"];
+$lastname = $_POST["comment"];
+$email = $_POST["email"];
 $stmt->execute();
 
 echo "New records created successfully";
 
 $stmt->close();
 $conn->close();
-?>
