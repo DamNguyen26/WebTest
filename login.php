774cd995eb2e23c4a35ed77f2db1b630e9947cd7
_damnguyen2604_ <dammit2525@yahoo.com.vn>
2021-08-29 10:27:19 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php 
+require_once("includes/function/connection.php");
+?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="welcome_get.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+</form>
+</body>
+</html>
