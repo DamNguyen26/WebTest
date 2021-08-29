6ca61eeee2d606a8d1d3ace3fea15a78b0f2d25c
_damnguyen2604_ <dammit2525@yahoo.com.vn>
2021-08-29 15:29:33 +0700

    Update createTable.php

--- includes/function/createTable.php
+++ includes/function/createTable.php
@@ -2,16 +2,17 @@
 require_once "connection.php";
 
 // Sql to create table
-$sql = "CREATE TABLE MyGuests (
+$sql = "CREATE TABLE Users (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     firstname VARCHAR(30) NOT NULL,
     lastname VARCHAR(30) NOT NULL,
-    email VARCHAR(50),
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
     reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
     )";
 
 if ($conn->query($sql) === true) {
-    echo "Table MyGuests created successfully";
+    echo "Table Users created successfully";
 } else {
     echo "Error creating table: " . $conn->error;
 }
