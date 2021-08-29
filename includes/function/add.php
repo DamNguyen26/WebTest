2e5b6915d4af1efd469f2e05a54f4086439a835e
_damnguyen2604_ <dammit2525@yahoo.com.vn>
2021-08-29 15:15:48 +0700

    Update add.php

--- includes/function/add.php
+++ includes/function/add.php
@@ -5,17 +5,18 @@ if (isset($_REQUEST["add"])) {
     $sql = "INSERT INTO MyGuests (firstname, lastname, email) VALUES (?, ?, ?)";
     // Prepare and bind
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("sss", $firstname, $lastname, $email);
-
-    // Set parameters and execute
-    $firstname = $_POST["firstname"];
-    $lastname = $_POST["lastname"];
-    $email = $_POST["email"];
-    $stmt->execute();
-
-    echo "New records created successfully";
-    echo '<a href="../../show.php">Show</a>';
-
+    if ($stmt) {
+        $stmt->bind_param("sss", $firstname, $lastname, $email);
+        // Set parameters and execute
+        $firstname = $_POST["firstname"];
+        $lastname = $_POST["lastname"];
+        $email = $_POST["email"];
+        $stmt->execute();
+        echo "New records created successfully";
+        echo '<a href="../../show.php">Show</a>';
+    } else {
+        echo "Error deleting record: " . $conn->error;
+    }
     $stmt->close();
     $conn->close();
 }
