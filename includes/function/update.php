0438debc8fa16d183208aaa2a082bc1330ad26c4
_damnguyen2604_ <dammit2525@yahoo.com.vn>
2021-08-29 12:50:26 +0700

    Create update.php

---
+++ includes/function/update.php
@@ -0,0 +1,21 @@
+<?php
+require_once ("connection.php");
+
+if (isset($_REQUEST['update'])){
+$sql = "UPDATE MyGuests SET id = ?, firstname = ?, lastname = ? WHERE id = ?";
+$stmt = $conn->prepare($sql);
+if ($stmt) {
+  $stmt->bind_param("issi", $id, $firstname, $lastname, $id);
+// Set parameters and execute
+$id = $_REQUEST['id'];
+$firstname = $_REQUEST['firstname'];
+$lastname = $_REQUEST['lastname'];
+$stmt->execute();
+echo "Record updated successfully";
+}else {
+  echo "Error updating record: " . $conn->error;
+}
+$stmt->close();
+$conn->close();
+}
+?>
