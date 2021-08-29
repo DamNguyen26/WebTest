380e56aabde56362633cbe03e6b80bd0f4aee860
_damnguyen2604_ <dammit2525@yahoo.com.vn>
2021-08-29 15:08:52 +0700

    Update add.php

--- includes/function/add.php
+++ includes/function/add.php
@@ -1,19 +1,21 @@
 <?php
 require_once "connection.php";
 
-// Prepare and bind
-$stmt = $conn->prepare(
-    "INSERT INTO MyGuests (firstname, lastname, email) VALUES (?, ?, ?)"
-);
-$stmt->bind_param("sss", $firstname, $lastname, $email);
+if (isset($_REQUEST["add"])) {
+    $sql = "INSERT INTO MyGuests (firstname, lastname, email) VALUES (?, ?, ?)";
+    // Prepare and bind
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sss", $firstname, $lastname, $email);
 
-// Set parameters and execute
-$firstname = $_POST["name"];
-$lastname = $_POST["comment"];
-$email = $_POST["email"];
-$stmt->execute();
+    // Set parameters and execute
+    $firstname = $_POST["firstname"];
+    $lastname = $_POST["lastname"];
+    $email = $_POST["email"];
+    $stmt->execute();
 
-echo "New records created successfully";
+    echo "New records created successfully";
+    echo '<a href="../../show.php">Show</a>';
 
-$stmt->close();
-$conn->close();
+    $stmt->close();
+    $conn->close();
+}
